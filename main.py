ae149467be5b0cbfab5e65d34ef1da7c34a25d14
Nina Husieva <nina.husieva2@dxc.com>
2025-01-14 00:08:52 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from contragent_service import ContragentService
+from cash_flow_service import CashFlowService
+
+
+if __name__ == "__main__":
+    contragent_service = ContragentService()
+    cash_flow_service = CashFlowService()
+
+    # Добавляем контрагента
+    contragent = contragent_service.add_contragent("Иван Иванов")
+    print(contragent)
+
+    # Добавляем доход
+    income = cash_flow_service.add_cash_flow("Зарплата", 50000, contragent, "income")
+    print(income)
+
+    # Добавляем расход
+    expense = cash_flow_service.add_cash_flow("Покупка", 10000, contragent, "expense")
+    print(expense)
+
+    # Печатаем баланс контрагента
+    print(f"Текущий баланс контрагента: {contragent.balance}")
